008dccabc86c8ea509d9b63f331789b06fc8c944
Guilherme Bazzo <bazzoguilherme@gmail.com>
2020-10-31 18:27:45 -0300

    Tentando iniciar E5

---
+++ Etapa5/gera_codigo.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "gera_codigo.h"
+
+#define MAX_TAM_REGIS 12
+#define MAX_TAM_LABEL 12
+
+int regis = 0, label = 0;
+
+char *gera_regis() {
+    char *new_regis = (char *) malloc (MAX_TAM_REGIS);
+    snprintf(new_regis, MAX_TAM_REGIS, "r%d", regis++);
+    return new_regis;
+}
+
+char *gera_label() {
+    char *new_label = (char *) malloc (MAX_TAM_LABEL);
+    snprintf(new_label, MAX_TAM_LABEL, "L%d", label++);
+    return new_label;
+}
+
+int main() {
+
+    printf("%s\n", gera_regis());
+    printf("%s\n", gera_regis());
+    printf("%s\n", gera_regis());
+
+
+    return 0;
+}
---
+++ Etapa5/gera_codigo.h
@@ -0,0 +1,9 @@
+
+#ifndef GERA_COD_H
+#define GERA_COD_H
+
+char *gera_regis();
+char *gera_label();
+
+
+#endif
